7f8e9088720e8997ab76e014b4b12ca719dc1c99
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-02-01 21:20:30 +0600

    removed unusued next config, tried to enable tsbuild error but it returns a false error caused by next auth.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -38,4 +34,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
